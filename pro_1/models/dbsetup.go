32ec78419fa5b6c1071c9999e8f2d420e450a76f
apurvisri <apurvisrivastava@192.168.1.4>
2023-09-03 12:53:47 +0530

    Added go application file with postgres connection

---
+++ pro_1/models/dbsetup.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+var MPosDB *sql.DB
+var MPosGORM *gorm.DB
+var err error
+
+func InitGormPostgres() {
+	MPosGORM, err = gorm.Open("postgres", "user=postgres dbname=calix_proj password=AniAnu2 sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
