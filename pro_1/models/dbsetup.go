e2bfd9b0c2d240faa5d64f92b5b96cba7dcb1d63
apurvisri <apurvisrivastava@192.168.1.4>
2023-09-03 15:44:50 +0530

    Added go application file with postgres connection

--- pro_1/models/dbsetup.go
+++ pro_1/models/dbsetup.go
@@ -14,7 +14,7 @@ var MPosGORM *gorm.DB
 var err error
 
 func InitGormPostgres() {
-	MPosGORM, err = gorm.Open("postgres", "user=postgres dbname=calix_proj password=AniAnu2 sslmode=disable")
+	MPosGORM, err = gorm.Open("postgres", "user=postgres dbname=calix_proj password=apurvi123 sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
